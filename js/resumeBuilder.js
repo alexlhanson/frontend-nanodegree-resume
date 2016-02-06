f7eea6acaf13e45580fec565c3456a8fe112e973
Alex Hanson <alexlhanson@gmail.com>
2016-02-05 22:57:18 -0800

    Modify the header section of resume

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,4 +1,10 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
- $("#main").append(["Alex Hanson"]);
+var formattedName = HTMLheaderName.replace("%data%", "Alex Hanson")
+var formattedRole = HTMLheaderRole.replace("%data%", "Environmental/Data Scientist")
+
+$("#header").prepend(formattedRole);
+
+$("#header").prepend(formattedName);
+
