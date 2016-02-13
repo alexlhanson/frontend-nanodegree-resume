8b7e4a1952ba328871956b1d741ead0bd0df681a
Alex Hanson <alexlhanson@gmail.com>
2016-02-12 21:45:19 -0800

    Add internationalize button to set last name uppercase

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -198,3 +198,13 @@ function displayWork(){
 }
 
 displayWork();
+
+function inName() {
+    var names = bio.name.split(" ");
+    names[0] = names[0].slice(0,1).toUpperCase() + names[0].slice(1).toLowerCase();
+    names[1] = names[1].toUpperCase();
+    var finalName = names.join(" ");
+    return finalName;
+}
+
+$("#main").append(internationalizeButton);
